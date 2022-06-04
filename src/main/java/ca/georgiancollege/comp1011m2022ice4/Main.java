83fc1c0e0400cf26c8e3554f6f06ad88fe40842d
Nitin <73417686+nitin4679@users.noreply.github.com>
2022-06-04 13:55:02 -0400

    added basic UI for table view

--- src/main/java/ca/georgiancollege/comp1011m2022ice4/Main.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice4/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("calculate-vector2d-distance.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("vector2d-tableview.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Calculate Vector2D Distance");
         stage.setScene(scene);
--- src/main/java/ca/georgiancollege/comp1011m2022ice4/Vector2DTableViewController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice4/Vector2DTableViewController.java
@@ -1,5 +1,31 @@
 package ca.georgiancollege.comp1011m2022ice4;
 
-public class Vector2DTableViewController
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Vector2DTableViewController implements Initializable
 {
+
+    @FXML
+    private TableView<Vector2D> tableView;
+    @FXML
+    private TableColumn<Vector2D, Integer> VectorIDColumn;
+
+    @FXML
+    private TableColumn<Vector2D, Float> XColumn;
+
+    @FXML
+    private TableColumn<Vector2D, Float> YColumn;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+
+    }
 }
